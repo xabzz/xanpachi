d76d856818f4f3569f8381b3c31b418457893481
xabzz <127979374+xabzz@users.noreply.github.com>
2023-10-10 10:37:17 -0400

    Add files via upload

---
+++ enter-script.js
@@ -0,0 +1,5 @@
+const enterButton = document.getElementById('enter-button');
+
+enterButton.addEventListener('click', () => {
+    window.location.href = 'index.html'; // Replace with your actual bio link page
+});
